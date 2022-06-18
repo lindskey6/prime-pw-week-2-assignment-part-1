4ec15ad4aece3c9e757849dd1afcbfb6fc367151
Linssi Colin Ruiz <linssi101@yahoo.com>
2022-06-18 01:36:18 -0500

    added updates to line 45

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -18,29 +18,32 @@ var luckyNumber = '6';
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
-console.log ("My name is  " + fullName + ", and I think", + luckyNumber + " is a winner!")
+console.log ("My name is  " + fullName + ", and I think", + luckyNumber + " is a winner!");
 
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-var adventurous = true
+var adventurous = true;
 
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
-var food = 'Hot Pot'
+var food = 'Hot Pot';
 
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-var pets = '2'
+var pets = '2';
 
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-var friendsPets = '2'
+var friendsPets = '2';
 
 // 11 - Add two pets to your `pets` variable
-let pet1 = 'Maya'
-let pet2 = 'Luna' 
+let pet1 = 'Maya';
+let pet2 = 'Luna';
 
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-const = '4'
+const allowed pets = 4 
 
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
+if adventurous == true;
+console.log ("adventures are great!")
+
 
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
